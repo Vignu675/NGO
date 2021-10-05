eac0fdfa9259e9851dc970306828bbbb1d4c5f9e
Vignu675 <54848324+Vignu675@users.noreply.github.com>
2021-10-05 16:48:45 +0530

    Add files via upload

---
+++ src/main/java/com/capgemini/payload/ValidationErrorResponse.java
@@ -0,0 +1,16 @@
+package com.capgemini.payload;
+
+import java.util.List;
+
+public class ValidationErrorResponse extends BaseErrorResponse {
+	private List<String> details;
+
+	public List<String> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<String> details) {
+		this.details = details;
+	}	
+
+}
